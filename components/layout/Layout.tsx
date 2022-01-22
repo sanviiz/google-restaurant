d17b55574ded885ab4f3a49a2665ee07d5bad089
test <sanviiz06@hotmail.com>
2022-01-23 02:40:31 +0700

    Add: `prompt` font family

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from '@layout/Header'
-import Footer from '@layout/Footer'
+import Header from '@components/Layout/Header'
+import Footer from '@components/Layout/Footer'
 
 const Layout = ({ children }) => {
 	return (
 		<>
-			<div className="font-sans antialiased">
+			<div className="font-prompt antialiased">
 				<Header />
 				<div className="container mx-auto pt-6 pb-20">{children}</div>
 				<Footer />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '@layout/Layout'
+import Layout from '@components/Layout/Layout'
 import { ContextWrapper } from '@hooks/context'
 
 function MyApp({ Component, pageProps }: AppProps) {
